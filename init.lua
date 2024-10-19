01f875863f9aaa4d4cd743a53a2c05f57014e58b
neqbal <nooraineqbal@gmail.com>
2024-10-20 00:59:10 +0530

    Tab

--- init.lua
+++ init.lua
@@ -24,8 +24,8 @@ local function set_relative_line_numbers()
     set_relative_line_numbers()
   end
 
-vim.opt.tabstop = 2
-vim.opt.shiftwidth = 2
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
-vim.bo.softtabstop = 2 
+vim.bo.softtabstop = 4
 vim.o.scrolloff = 999
